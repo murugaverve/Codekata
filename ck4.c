6f3cf0c16c1e79129c52b62ec24604cb300a2b68
Vasanth0347 <vasanth0347@gmail.com>
2016-07-21 18:55:53 +0530

    Create ck4.c

---
+++ ck4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    if((c>=65 && c<=90) || (c>=97 && c<=122))
+      printf("Alphabet");
+    else
+      printf("Not an Alphabet");
+    return 0;
+}
