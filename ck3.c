3693a9b240fd8c0b97d0f746334c8252165f5eb3
Vasanth0347 <vasanth0347@gmail.com>
2016-07-21 18:48:24 +0530

    Create ck3.c

---
+++ ck3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  char c;
+  scanf("%c",&c);
+  if(c==65 || c==69 || c==73 || c==79 || c==85 || c==97 || c==101 || c==105 || c==111 || c==117)
+    printf("Vowel");
+  else
+    printf("Consonant");
+  return 0;
+}
