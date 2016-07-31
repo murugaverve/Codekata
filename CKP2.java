92142af3965e9542f3b98b7c91460d8160a4c184
Vasanth0347 <vasanth0347@gmail.com>
2016-07-31 19:36:54 +0530

    Create CKP2.java

---
+++ CKP2.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class minimum 
+{
+public static void main(String ar[])throws Exception
+{
+    int r,i,k;
+    int a[]=new int[100];
+Scanner s=new Scanner(System.in);
+minimum m1=new minimum();
+String p=new String();
+p=s.next();
+k=s.nextInt();
+int n=Integer.parseInt(p);
+i=0;
+while(n>0)
+{
+r=n%10;
+a[i++]=r;
+n/=10;
+}
+m1.sort(a,k,i);
+}
+public void sort(int l[],int m,int g)
+{
+    int t;
+for(int i=0;i<g-1;i++)
+{
+for(int j=i+1;j<g;j++)
+{
+if(l[i]>l[j])
+{
+t=l[i];
+l[i]=l[j];
+l[j]=t;
+}
+}
+}
+for(int i=0;i<m;i++)
+    System.out.print(l[i]);
+}
+}
