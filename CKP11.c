950c6d12f552ee5a671ec86bb860bca6b3912c8e
Vasanth0347 <vasanth0347@gmail.com>
2016-08-03 15:49:16 +0530

    Create CKP11.c

---
+++ CKP11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,n,i,j,count=0;
+scanf("%d",&a);
+n=2*a;
+for(i=1;i<n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+printf("(%d,%d)\n",i,j);
+count++;
+}
+}
+printf("No of pairs =%d",count);
+return 0;
+}
