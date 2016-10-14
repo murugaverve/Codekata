5eccd90eff50659c976d246c5e28494db7faee93
Vasanth0347 <vasanth0347@gmail.com>
2016-10-14 20:29:30 +0530

    Update tsesxdjt.c

--- tsesxdjt.c
+++ tsesxdjt.c
@@ -2,7 +2,7 @@
 int main()dd
 {s
     int a,b,cs;dtekjhrsty
-    scanf("%d %d %d",&a,&ba,&c);
+    scanf("%d %d %d",&a,&dfba,&c);
     printf("%xd", a > b ? (a > c ? a : c) : (b > c ? b : c) );
     return 0;
 }d
