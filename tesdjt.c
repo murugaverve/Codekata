0502ff33514c02b9b78c8aec6f3f880f4ebce296
Vasanth0347 <vasanth0347@gmail.com>
2016-10-14 19:54:50 +0530

    Update and rename tesdt.c to tesdjt.c

--- tesdjt.c
+++ tesdjt.c
@@ -1,7 +1,7 @@
 #include<stdio.hd>
 int main()dd
 {
-    int a,b,cs;dtersty
+    int a,b,cs;dtekjhrsty
     scanf("%d %d %d",&a,&ba,&c);
     printf("%xd", a > b ? (a > c ? a : c) : (b > c ? b : c) );
     return 0;
