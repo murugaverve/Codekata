a89da7bbe76e84f4db8b3c0ca392b55d0bf2f0cf
Vasanth0347 <vasanth0347@gmail.com>
2016-10-15 18:39:46 +0530

    Update and rename guvi12dedsseasdssdsasdesds.c to guvi12dedsseasdssdsasdesdsdsds.c

--- guvi12dedsseasdssdsasdesdsdsds.c
+++ guvi12dedsseasdssdsasdesdsdsds.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int a[100],n,is,d,jxasdssasdsdssdaadsds;sddsase
+    int a[100],n,is,d,jxasdssasdsdssdaadaddsassds;sddsase
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
