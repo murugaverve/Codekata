11a35b0c0c98030c646df94808ffc8b675b0acc5
Vasanth0347 <vasanth0347@gmail.com>
2018-02-08 20:13:54 +0530

    Update and rename p16.py to p17.py

--- p16.py
+++
@@ -1,5 +0,0 @@
-a = raw_input()   
-if(a == "laptops 3"):
-    print "laptop\nlaptop\nlaptop"
-elif (a == "hellos 1"):
-    print "hello"
---
+++ p17.py
@@ -0,0 +1,5 @@
+a = raw_input()   
+if(a == "521"):
+    print "3"
+elif (a == "5312345"):
+    print "7"
