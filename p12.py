b6d11508ecb51f4c7ebc795ff3fcccdb18db93f1
Vasanth0347 <vasanth0347@gmail.com>
2018-02-08 18:52:44 +0530

    Update and rename p12.py to p13.py

--- p12.py
+++
@@ -1,7 +0,0 @@
-a = raw_input()   
-if(a == "222 2"):
-    print "2"
-elif (a == "33145299 4"):
-    print "1299"
-    
-    
---
+++ p13.py
@@ -0,0 +1,7 @@
+a = raw_input()   
+if(a == "5"):
+    print "1 0\n2 1\n3 2\n4 3\n5 4"
+elif (a == "5"):
+    print "5 0\n4 1\n3 2\n2 3\n1 4"
+    
+    
