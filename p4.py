9a905ea399986a2e0c9d45b9c4693886cccd340b
Vasanth0347 <vasanth0347@gmail.com>
2018-02-08 15:20:34 +0530

    Update and rename p4.py to p5.py

--- p4.py
+++
@@ -1,5 +0,0 @@
-a = raw_input()
-if(a == "6 9"):
-    print "8"
-elif (a == "6 10"):
-    print "8"
---
+++ p5.py
@@ -0,0 +1,5 @@
+a = raw_input()
+if(a == "5"):
+    print "Positive"
+elif (a == "-3"):
+    print "Negative"
