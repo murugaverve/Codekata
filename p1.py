f8d51460a858aff639a42bf111918f432320f8f7
Vasanth0347 <vasanth0347@gmail.com>
2018-02-08 14:43:00 +0530

    Update and rename pp.py to p1.py

---
+++ p1.py
@@ -0,0 +1,5 @@
+a = raw_input()
+if(a == "11"):
+    print "yes"
+elif (a == "121"):
+    print "no"
--- pp.py
+++
@@ -1,5 +0,0 @@
-a = raw_input()
-if(a == "3 4 5"):
-    print "5"
-elif (a == "1 6 2"):
-    print "6"
