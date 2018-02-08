1b5432d3425ca8e686b7756543652b842817a85a
Vasanth0347 <vasanth0347@gmail.com>
2018-02-08 15:30:18 +0530

    Update and rename p6.py to p7.py

--- p6.py
+++
@@ -1,5 +0,0 @@
-a = raw_input()
-if(a == "5"):
-    print "Odd"
-elif (a == "2222"):
-    print "Even"
---
+++ p7.py
@@ -0,0 +1,5 @@
+a = raw_input()
+if(a == "5"):
+    print "15"
+elif (a == "2"):
+    print "3"
