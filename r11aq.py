2e06bee9cdcfec362627b7a7e21d911de51abc5c
Vasanth0347 <vasanth0347@gmail.com>
2018-05-24 18:25:33 +0530

    Update and rename r11aq.py to rs11aq.py

--- r11aq.py
+++
@@ -1,5 +0,0 @@
-a = raw_input()
-if a == ("1"):
-  print ("One")
-elif (a == "2"):
-  print "Two"
---
+++ rs11aq.py
@@ -0,0 +1,5 @@
+a = raw_input()
+if a == ("123"):
+  print ("6")
+elif (a == "23456"):
+  print "20"
