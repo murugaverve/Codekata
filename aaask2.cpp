75786468120fe58463d3004740535d2e64125734
Vasanth0347 <vasanth0347@gmail.com>
2018-05-25 14:24:25 +0530

    Update and rename caaask2.c to aaask2.cpp

---
+++ aaask2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int a,i;
+cin>>a;
+for(i=0;i<a;i++){
+	cout<<"Hello\n";
+}
+	return 0;
+}
--- caaask2.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
- printf("even");
- return 0;
-}
