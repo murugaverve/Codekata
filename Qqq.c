3a6ba8bf851e4687caf616b0b929df3500c3040c
Vasanth0347 <vasanth0347@gmail.com>
2018-05-28 11:59:26 +0530

    Update and rename q1qq.c to Qqq.c

---
+++ Qqq.c
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << "12"; 
+   
+   return 0;
+}
--- q1qq.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-   printf("112");
- return 0;
-}
