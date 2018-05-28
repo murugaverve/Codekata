712cbca3d702ec682f697e4dd19a3f94a88954f9
Vasanth0347 <vasanth0347@gmail.com>
2018-05-28 14:56:10 +0530

    Update and rename zaz.clj to zzaz.js

--- zaz.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure.examples.hello
-	(:gen-class))
-
-(defn hello-world [username]
-(println (format "123" )))
-
-(hello-world "world")
---
+++ zzaz.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<title>Web Page Design</title>
+<head>
+<script>
+function sayHello() {
+   document.write("123");
+}
+sayHello();
+</script>
+</head>
+<body>
+</body>
+</html>
