f6e0cc004f246ec23240023284f92ebc08466fcd
Vasanth0347 <vasanth0347@gmail.com>
2018-05-30 14:30:15 +0530

    Update and rename azaadsaq.c to vsaq.c

--- azaadsaq.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main(){
-printf("asda");
-
-return 0;}
---
+++ vsaq.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int num;
+scanf("%d",&num);
+if(num==3){
+printf("3");}
+if(num==2){
+printf("2");}else{
+printf("123");}
+return 0;}
