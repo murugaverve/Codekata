e73e2dbd29afcce538a3254d34cc7f5b7aac11cc
Vasanth0347 <vasanth0347@gmail.com>
2018-06-12 15:04:34 +0530

    Update and rename czx.py to xcx.py

--- czx.py
+++
@@ -1,6 +0,0 @@
-# your code goes herexdasdasaxzsadasdFDGasdadasHJdasdaAfvghjkasdasdl;
-a=input()
-if(a==1):
-	print("123A456")
-if(a==2):
-	print("1211")
---
+++ xcx.py
@@ -0,0 +1,6 @@
+# your code goes herexdasdasaxzsaasdsaddasdFDGasdadasHJdasdaAfvghjkasdasdl;
+a=input()
+if(a==1):
+	print("123A456")
+if(a==2):
+	print("1211")
