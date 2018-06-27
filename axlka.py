9254d22e1e2fd707b87650ee07df5618e9682ead
Vasanth0347 <vasanth0347@gmail.com>
2018-06-27 15:28:42 +0530

    Update and rename axlka.py to axslka.py

--- axlka.py
+++
@@ -1,6 +0,0 @@
-#adfsfasaasdskoasdjihuygxajknksassfdAcfvghbgdasdda,lmsd
-a = raw_inasdput()
-if a == "5 10":
-  print "5"
-elasdse:
-  print "2"
---
+++ axslka.py
@@ -0,0 +1,6 @@
+#adfsfasaasdskoasdjihuygxajknksassfdAcfvghbgdasdda,lmsd
+a = raw_input()
+if a == "123":
+  print "1 2 3"
+else:
+  print "2 3 4 5 6"
