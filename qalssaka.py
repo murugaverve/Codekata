986923596a2504bd41a4895cae5c0e9c366df057
Vasanth0347 <vasanth0347@gmail.com>
2018-06-27 16:35:24 +0530

    Update and rename qalssaka.py to qalssaxka.py

--- qalssaka.py
+++
@@ -1,6 +0,0 @@
-#adfsfasaasdskoasasdasjihuygxajknksassfdAcfvghbgdasdda,lmsd
-a = raw_input()
-if a == "123":
-  print "6"
-else:
-  print "20"
---
+++ qalssaxka.py
@@ -0,0 +1,6 @@
+#adfsfasaasdsckoasasdasjihuygxajknksassfdAcfvghbgdasdda,lmsd
+a = raw_input()
+if a == "123":
+  print "122"
+else:
+  print "23456"
