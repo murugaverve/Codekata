1c0222b80ab23082f0643c207b2c3b8690a4f515
Vasanth0347 <vasanth0347@gmail.com>
2018-07-02 16:41:54 +0530

    Update and rename qx1sa.py to qxs1sa.py

--- qx1sa.py
+++
@@ -1,6 +0,0 @@
-#a12asasdasdasssd3xsd
-a = raw_input()
-if a == "123":
-  print "1 2 3"
-else:
-  print "Yes"
---
+++ qxs1sa.py
@@ -0,0 +1,6 @@
+#a12asasdasdasssd3xsd
+a = raw_input()
+if a == "521":
+  print "3"
+else:
+  print "7"
