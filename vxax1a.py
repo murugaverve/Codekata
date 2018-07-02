e19ab061d3cf2e307eda2cecb0646baaae8465e4
Vasanth0347 <vasanth0347@gmail.com>
2018-07-02 14:26:54 +0530

    Update and rename vxax1a.py to vxxax1a.py

--- vxax1a.py
+++
@@ -1,6 +0,0 @@
-#a12asasddasd3xsd
-a = raw_input()
-if a == "aabbcc":
-  print "ccbbaa"
-else:
-  print "dcba"
---
+++ vxxax1a.py
@@ -0,0 +1,6 @@
+#a12asasddasd3xsd
+a = raw_input()
+if a == "2 3":
+  print "8"
+else:
+  print "4"
