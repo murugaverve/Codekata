cf633e96cb19faca21c99655f7480bd5248b1405
Vasanth0347 <vasanth0347@gmail.com>
2018-07-06 19:22:21 +0530

    Update and rename countvarun.py to testcode.py

--- countvarun.py
+++
@@ -1,5 +0,0 @@
-b=input()
-if(b==521):
-	print("3")
-if(b==5312345):
-	print("7")
---
+++ testcode.py
@@ -0,0 +1,9 @@
+def main():
+    n=3
+    if(n%10):
+        n=n+(10-n%10)
+        print(n)
+ pass
+
+if __name__ == '__main__':
+main()
