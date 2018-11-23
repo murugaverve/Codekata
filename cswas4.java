bba88efc55d9caae8e78a98a56abce8291512d82
Vasanth0347 <vasanth0347@gmail.com>
2018-11-23 11:18:17 +0530

    Update and rename cswa4.java to cswas4.java

---
+++ cswas4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class EqualNoofbraces {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		braces(str);
+
+	}
+
+	private static void braces(String str) {
+		int count=0;
+		int count1=0;
+		for(int i=0;i<str.length();i++){
+			if(str.charAt(i)=='('){
+				count++;
+			}
+			else if(str.charAt(i)==')'){
+				count1++;
+				}
+			}
+			if(count1==count){
+				System.out.println("yes");}
+			else{
+				System.out.println("no");
+			}
+          //asdas
+	}
+}
