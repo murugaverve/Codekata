658f693cb816234771b26ba000251d2ef0b79dd5
murugaverve <muruga.click@gmail.com>
2019-04-10 15:46:44 +0530

    Create mt1.java

---
+++ mt1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class stringrepeatjava 
+{  
+
+public static void main(String args[])  
+{   
+
+Scanner input=new Scanner(System.in);
+String strENG=input.next();
+int count=input.nextInt();         
+ for(int i=0;i<count;i++){System.out.println(strENG);} 
+}
+}
